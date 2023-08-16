76781749c0b19fba9eb72ff7025829aafb57ab47
Garrett Gorman <garrettgorman1@gmail.com>
2023-08-16 15:05:02 -0400

    Cleaned up code

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 
 
 const flavanator = () => {
-const userInput = prompt("Enter a list of comma seperated froyo flavors", "vanilla,vanilla,vanilla,strawberry,coffee,coffee")
-const froyoFlavors = userInput.split(",")
-const froyoObject = {}
-for (let i = 0; i < froyoFlavors.length; i++){
-    if (!froyoObject[froyoFlavors[i]]){
-        froyoObject[froyoFlavors[i]] = 1;
-    } else if (froyoObject[froyoFlavors[i]]){
-        froyoObject[froyoFlavors[i]] ++
+    const userInput = prompt("Enter a list of comma seperated froyo flavors", "vanilla,vanilla,vanilla,strawberry,coffee,coffee")
+    const froyoFlavors = userInput.split(",")
+    const froyoObject = {}
+    for (let i = 0; i < froyoFlavors.length; i++){
+        if (!froyoObject[froyoFlavors[i]]){
+            froyoObject[froyoFlavors[i]] = 1;
+        } else if (froyoObject[froyoFlavors[i]]){
+            froyoObject[froyoFlavors[i]] ++
+        }
     }
-}
-return froyoObject
+    return froyoObject
 }
 
 
