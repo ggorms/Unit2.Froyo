343506111cc8da20c834548e063eb6e9f3c4a84a
Garrett Gorman <garrettgorman1@gmail.com>
2023-08-16 15:02:47 -0400

    Final Commit

---
+++ index.js
@@ -0,0 +1,19 @@
+
+
+const flavanator = () => {
+const userInput = prompt("Enter a list of comma seperated froyo flavors", "vanilla,vanilla,vanilla,strawberry,coffee,coffee")
+const froyoFlavors = userInput.split(",")
+const froyoObject = {}
+for (let i = 0; i < froyoFlavors.length; i++){
+    if (!froyoObject[froyoFlavors[i]]){
+        froyoObject[froyoFlavors[i]] = 1;
+    } else if (froyoObject[froyoFlavors[i]]){
+        froyoObject[froyoFlavors[i]] ++
+    }
+}
+return froyoObject
+}
+
+
+
+console.log(flavanator())
